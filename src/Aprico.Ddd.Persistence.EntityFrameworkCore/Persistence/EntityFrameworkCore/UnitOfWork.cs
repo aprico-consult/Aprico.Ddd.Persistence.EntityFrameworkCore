59814ebb216b619cbb61ffaac451658c922a7a2b
François Chabot <francois.chabot@stateless.be>
2025-02-25 22:36:58 +0100

    Add UnitOfWorkFactory implementation and update UnitOfWork base class
    Introduce `UnitOfWorkFactory` to standardize UnitOfWork creation using a provided DbContext, adhering to the IUnitOfWorkFactory pattern. Update `UnitOfWork` to extend `UnitOfWorkBase` for better abstraction and consistency.

--- src/Aprico.Ddd.Persistence.EntityFrameworkCore/Persistence/EntityFrameworkCore/UnitOfWork.cs
+++ src/Aprico.Ddd.Persistence.EntityFrameworkCore/Persistence/EntityFrameworkCore/UnitOfWork.cs
@@ -51,7 +51,7 @@ namespace Aprico.Persistence.EntityFrameworkCore;
 /// ]]></code>
 /// </example>
 [SuppressMessage("ReSharper", "MemberCanBeInternal", Justification = "Public API.")]
-public class UnitOfWork : Abstractions.UnitOfWork
+public class UnitOfWork : UnitOfWorkBase
 {
 	public UnitOfWork(DbContext dbContext)
 	{
