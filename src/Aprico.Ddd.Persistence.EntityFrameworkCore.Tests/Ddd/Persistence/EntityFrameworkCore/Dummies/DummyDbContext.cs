cd6db6dfca0c18329f97e2183f66ab34290e548a
François Chabot <francois.chabot@stateless.be>
2025-02-25 22:07:16 +0100

    Rename 'Aggregates' to 'Dummies' in DummyDbContext.
    Updated the property name in DummyDbContext to better represent the entity set being managed. This change improves code clarity and aligns with naming conventions. No functional impact anticipated.

--- src/Aprico.Ddd.Persistence.EntityFrameworkCore.Tests/Ddd/Persistence/EntityFrameworkCore/Dummies/DummyDbContext.cs
+++ src/Aprico.Ddd.Persistence.EntityFrameworkCore.Tests/Ddd/Persistence/EntityFrameworkCore/Dummies/DummyDbContext.cs
@@ -1,13 +1,13 @@
 #region Copyright & License
 
 // Copyright © 2024 - 2025 Aprico Consultants
-// 
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-// 
+//
 // http://www.apache.org/licenses/LICENSE-2.0
-// 
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -25,5 +25,5 @@ namespace Aprico.Ddd.Persistence.EntityFrameworkCore.Dummies;
 internal sealed class DummyDbContext() : DbContext(new DbContextOptionsBuilder<DummyDbContext>().UseInMemoryDatabase(nameof(DummyDbContext)).Options)
 {
 	[SuppressMessage("ReSharper", "UnusedMember.Global")]
-	public DbSet<DummyAggregate> Aggregates { get; set; }
+	public DbSet<DummyAggregate> Dummies { get; set; }
 }
